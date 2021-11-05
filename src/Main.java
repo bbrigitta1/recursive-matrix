86859e074a4e42f18f42ea430a9013e377aead76
Brigitta Bódi <brigi9222@gmail.com>
2021-11-05 11:01:26 +0100

    display time

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class Main {
   public static void main(String[] args) {
+    // 1. print the matrix
     Matrix matrix = new Matrix();
     matrix.printMatrix();
+    System.out.println();
+
+    // 2.print actual time
+    LocalDateTime actualDateTime = LocalDateTime.now();
+    DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyy hh:mm:ss");
+    System.out.println(actualDateTime.format(format));
   }
 }
